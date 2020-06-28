bbc634354c4d7319b3b86470b94b98afd95cd0f9
qw <123@163.com>
2020-06-28 19:49:50 +0800

    阶段提交

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,11 +1,92 @@
 import React from 'react';
+import Button from '@/components/Button';
 
 import './index.less';
 
-interface Iprops {}
+interface Iprops {
+  children?: any;
+  visible?: boolean;
+  onOk?: () => void;
+  onCancel?: () => void;
+  onClose?: () => void;
+  hasMask?: boolean;
+  title?: React.ReactNode;
+  style?: object;
+  hasFooter?: boolean;
+  footerContent?: React.ReactNode[];
+  footerAlign?: 'left' | 'center' | 'right';
+}
 
-const Dialog: React.FC<Iprops> = ({ children }) => {
-  return <>{children}</>;
+const Dialog: React.FC<Iprops> = ({
+  children,
+  visible = false,
+  onOk,
+  onCancel,
+  onClose,
+  hasMask = true,
+  title,
+  style,
+  hasFooter = true,
+  footerContent = [],
+  footerAlign = 'left',
+}) => {
+  let delFooterAlign = '';
+
+  switch (footerAlign) {
+    case 'left':
+      delFooterAlign = 'flex-start';
+      break;
+    case 'right':
+      delFooterAlign = 'flex-end';
+      break;
+    case 'center':
+      delFooterAlign = 'center';
+      break;
+  }
+
+  return (
+    <>
+      {visible && (
+        <div className="fish_dialog">
+          {hasMask && <div className="mask"></div>}
+          <div
+            className="dialog_content"
+            style={JSON.stringify(style) === '{}' ? {} : style}
+          >
+            <div className="top">
+              <div className="title">{title}</div>
+              <div className="close" onClick={() => onClose && onClose()}>
+                关闭
+              </div>
+            </div>
+            <div className="content">{children}</div>
+            {hasFooter && (
+              <div
+                className="footer"
+                style={{ justifyContent: delFooterAlign }}
+              >
+                {footerContent.length > 0 ? (
+                  <>
+                    {footerContent.map(item => {
+                      return item;
+                    })}
+                  </>
+                ) : (
+                  <>
+                    <Button type="primary" onClick={() => onOk && onOk()}>
+                      确认
+                    </Button>
+                    &nbsp;
+                    <Button onClick={() => onCancel && onCancel()}>取消</Button>
+                  </>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Dialog;
--- src/pages/dialog/index.tsx
+++ src/pages/dialog/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dialog from '@/components/Dialog';
+import Button from '@/components/Button';
 
 export default () => {
+  const [visible, setVisible] = useState(false);
   return (
     <>
-      <Dialog>qwewq</Dialog>
+      <Button onClick={() => setVisible(true)}>open</Button>
+      <Dialog
+        hasMask={true}
+        title="nisidisdqweqweqwewqe"
+        visible={visible}
+        hasFooter={true}
+        // footerAlign='right'
+        // footerContent={[<Button>12321</Button>, <Button>12321</Button>]}
+        style={{ width: '500px' }}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        onClose={() => setVisible(false)}
+      >
+        qwewq
+      </Dialog>
     </>
   );
 };
