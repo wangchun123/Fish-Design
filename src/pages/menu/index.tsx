9c510efd0a4cf11724f0e5a375ca9a74e78e8c03
qw <123@163.com>
2020-07-03 13:27:16 +0800

    Menu-style

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -3,13 +3,15 @@ import Menu from '@/components/Menu';
 
 export default () => {
   return (
-    <Menu
-      data={[
-        { url: '123', title: 'qwe', key: '1' },
-        { url: '123', title: 'qwe', key: '2' },
-      ]}
-      onChange={(item, index) => console.log(item, index)}
-      defaultSelectedKeys="2"
-    />
+    <div className="test_menu">
+      <Menu
+        data={[
+          { url: '123', title: 'qwe', key: '1' },
+          { url: '123', title: 'qwe', key: '2' },
+        ]}
+        onChange={(item, index) => console.log(item, index)}
+        defaultSelectedKeys="2"
+      />
+    </div>
   );
 };
