6ff2df170484d4124906642fcdeccef87a82a1e1
qw <123@163.com>
2020-07-04 13:03:11 +0800

    loding

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,12 +3,72 @@ import Loding from '@/image/loading/loding.gif';
 
 import './index.less';
 
-interface Iprops {}
-const Loading: React.FC<Iprops> = () => {
+interface Iprops {
+  /**loading 的接受值 */
+  children?: React.ReactNode;
+  /**loading 的显隐 */
+  visible?: boolean;
+  /**loading 的全屏模式 */
+  fullScreen?: boolean;
+  /**loading 的大小*/
+  size?: 'small' | 'medium' | 'large';
+  /**loading 的定制 */
+  lodingIcon?: React.ReactNode;
+}
+
+const Loading: React.FC<Iprops> = ({
+  visible = true,
+  fullScreen = false,
+  children,
+  size = 'small',
+  lodingIcon,
+}) => {
+  let lodingSize = '';
+  switch (size) {
+    case 'small':
+      lodingSize = '12px';
+      break;
+    case 'medium':
+      lodingSize = '24px';
+      break;
+    case 'large':
+      lodingSize = '48px';
+      break;
+  }
+
   return (
-    <div className="fish_loading">
-      <img src={Loding} alt="" style={{ height: '12px' }} />
-    </div>
+    <>
+      {visible ? (
+        <div className="fish_loading">
+          <div className="fish_loading_content">
+            <div style={{ opacity: '0.2' }}>{children}</div>
+            <div
+              className={
+                fullScreen
+                  ? 'fish_loading_mask_full'
+                  : 'fish_loading_mask_default'
+              }
+            ></div>
+
+            <div
+              className={
+                fullScreen
+                  ? 'fish_loading_icon_full'
+                  : 'fish_loading_icon_default'
+              }
+            >
+              <img
+                src={lodingIcon ? lodingIcon : Loding}
+                alt=""
+                style={{ height: lodingSize }}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>{children}</>
+      )}
+    </>
   );
 };
 
--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -4,7 +4,21 @@ import Loading from '@/components/Loading';
 export default () => {
   return (
     <>
-      <Loading />
+      <p>1.基本用法</p>
+      <Loading size="large">
+        <div
+          style={{
+            height: '200px',
+            width: '200px',
+            textAlign: 'center',
+            lineHeight: '200px',
+            background: 'red',
+          }}
+          onClick={() => alert(1)}
+        >
+          testqweqweqwewqe
+        </div>
+      </Loading>
     </>
   );
 };
